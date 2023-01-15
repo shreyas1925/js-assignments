d7c605e55c9eb1d284285d4440e3f540903e5dcb
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-15 19:24:05 +0530

    feat: add endsWithScript method to check ehether a string ends with Script along with test cases

---
+++ question6/question6.js
@@ -0,0 +1,10 @@
+function endsWithScript(string)
+{
+    return string.endsWith("Script")
+}
+
+const endsWithScript = (string) => string.endsWith("Script")
+
+endsWithScript("JavaScript")
+
+module.exports = endsWithScript
---
+++ question6/test.js
@@ -0,0 +1,24 @@
+const endsWithScript = require("./question6")
+
+describe('Tests for question 5',()=>{
+    test('test case one',()=>{
+        
+        expect(endsWithScript("javaS")).toBeFalsy()
+    })
+
+    test('test case two',()=>{
+        expect(endsWithScript("script")).toBeFalsy()
+        
+    })
+
+    test('test case three',()=>{
+       
+        expect(endsWithScript("ECMAScript")).toBeTruthy()
+    })
+
+    test('test case four',()=>{
+       
+        expect(endsWithScript("JavaScript")).toBeTruthy()
+    })
+}
+)
