3121665879b00ab209d6c5cf7228a85a4bf2d2a8
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-15 20:00:53 +0530

    feat: add numberOfVowels method to count vowels in string  along with test cases

---
+++ question8/test.js
@@ -0,0 +1,24 @@
+const numberOfVowels = require("./question8")
+
+describe('Tests for question 5',()=>{
+    test('test case one',()=>{
+        
+        expect(numberOfVowels("codeacademy")).toBe(5)
+    })
+
+    test('test case two',()=>{
+        expect(numberOfVowels("shreyas")).toBe(2)
+        
+    })
+
+    test('test case three',()=>{
+       
+        expect(numberOfVowels("appleeown")).toBe(4)
+    })
+
+    test('test case four',()=>{
+       
+        expect(numberOfVowels("okgooglee")).toBe(5)
+    })
+}
+)
