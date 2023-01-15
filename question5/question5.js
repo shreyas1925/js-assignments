585b066a1856eab65bc565a93ef0646fc47fe7fd
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-15 19:20:28 +0530

    feat: add isLeapYear method to check whether a year is leap year or not along with test cases

--- question5/question5.js
+++ question5/question5.js
@@ -3,11 +3,9 @@ function isLeapYear(year)
     return year%4==0 && year%100!=0 || year%400==0
 }
 
-isLeapYear(2004)
 
-const isLeapYear = (year) => {
-    return year%4==0 && year%100!=0 || year%400==0
-}
+const isLeapYear = (year) =>  year%4==0 && year%100!=0 || year%400==0
+
 
 isLeapYear(2004)
 
