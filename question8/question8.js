66feefaab60623216eb2f9d716324efe797f78ac
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-15 20:01:34 +0530

    feat: add numberOfVowels method to count vowels in string  along with test cases

--- question8/question8.js
+++ question8/question8.js
@@ -14,6 +14,22 @@ function numberOfVowels(string)
     return vowelcount
 }
 
+const numberOfVowels = (string) =>
+{
+    string = string.toLowerCase()
+    let vowelcount = 0
+
+    for(let i = 0 ;i <string.length;i++)
+    {
+        if(string.charAt(i)=='a' || string.charAt(i)=='e'|| string.charAt(i)=='i' || string.charAt(i)=='o' || string.charAt(i)=='u' || string.charAt(i)=='a' )
+        {
+            vowelcount++
+        }
+    }
+
+    return vowelcount
+}
+
 numberOfVowels("codeacademy")
 
 module.exports = numberOfVowels
